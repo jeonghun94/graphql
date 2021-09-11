b9232fffd9f585047c2ee1dec89bd5dd203765fd
jeonghun94 <66016555+jeonghun94@users.noreply.github.com>
2021-09-11 21:08:31 +0900

    수정

--- graphql/db.js
+++ graphql/db.js
@@ -4,16 +4,11 @@ let API_URL = "https://yts.am/api/v2/list_movies.json?";
 
 export const getMovies = async (rating, limit) => {
 
-    // if (rating > 0) {
-    //     API_URL += `rating=${rating}`;
-    // }
-
     if (limit > 0) {
         API_URL += `&limit=${limit}&sort_by=title`;
     }
 
     const { data: { data: { movies } } } = await axios.get(API_URL);
-    
     return movies;
 };
 
